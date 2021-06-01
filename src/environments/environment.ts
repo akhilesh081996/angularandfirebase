7f5063728f1e7c5c74fd8207714d1f3063314d1e
unknown <akhilesh.sharma.dev@gmail.com>
2021-06-01 14:25:27 +0530

    'updated'

---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+export const environment = {
+  production: false,
+  // firebase: {
+  //   apiKey: 'xxx',
+  //   authDomain: 'angular-10-crud-db.firebaseapp.com',
+  //   databaseURL: 'https://angular-10-crud-db.firebaseio.com',
+  //   projectId: 'angular-10-crud-db',
+  //   storageBucket: 'angular-10-crud-db.appspot.com',
+  //   messagingSenderId: 'xxx',
+  //   appId: '1:xxx:web:xxx'
+  // }
+  firebase : {
+    apiKey: "AIzaSyA4qn-_Te7dS3L4LftG2k8t6FK9JvnRtkg",
+    authDomain: "demofirebaseimageupload.firebaseapp.com",
+    projectId: "demofirebaseimageupload",
+    databaseURL: 'https://demofirebaseimageupload-default-rtdb.firebaseio.com',
+    storageBucket: "demofirebaseimageupload.appspot.com",
+    messagingSenderId: "472434592597",
+    appId: "1:472434592597:web:747d6982be1c4cfdd23ddc",
+   
+  }
+
+};
